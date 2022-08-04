6e96f7af75e808fd42cd18fc58cdded56f421919
VisheshPandey <visheshpandey24@gmail.com>
2022-08-04 16:50:37 +0530

    get multiple search results

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ImageItem from './ImageItem'
 import loading from './loading.gif'
 import search from './search.png'
 
@@ -6,32 +7,27 @@ import search from './search.png'
 function Home() {
 
     const [text, setText] = useState("")
-    const [imageAddress, setImageAddress] = useState("https://vishesh-pandey.github.io/v-images/static/media/search.12a4a3b77692113e2d59.png")
+    const [imageArray, setImageArray] = useState([])
 
     async function getImage() {
-        let info = await fetch(`https://source.unsplash.com/random/500×500/?${text}`)
-        let imageUrl = info['url']
-        setImageAddress(imageUrl)
-        document.getElementById("getImage").src = imageUrl
-    }
+        document.getElementById("searchSymbol").src = loading
+        const url = `https://api.unsplash.com/search/photos?query=${text}&client_id=CUoMn8YRFJuKjER5BQdzrVCGIwM1PTeACLuWWZGfzwg`
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        setImageArray(parsedData.results)
+        if (imageArray.length > 9) {
+            document.getElementById("searchSymbol").style.display = "none"
+        }
+        else {
+            document.getElementById("searchSymbol").src = search
+            document.getElementById("searchSymbol").style.display = "inline-block"
+            alert("Enter proper keyword")
 
-    function loadImage() {
-        getImage()
-        document.getElementById("getImage").src = loading
-        document.getElementById("copyImgAdd").innerText = "Copy Image Address";
+        }
     }
 
     const handleOnChange = (event) => {
         setText(event.target.value)
-    }
-
-    function confirmCopied() {
-        document.getElementById("copyImgAdd").innerText = "Image Address Copied !";
-    }
-
-    function copyImageAddress() {
-        navigator.clipboard.writeText(imageAddress);
-        confirmCopied();
     }
 
     return (
@@ -45,23 +41,24 @@ function Home() {
                                 <input className='w-100 h-100 fs-3 px-2 rounded' type="text" value={text} onChange={handleOnChange} placeholder='Write keyword here...' />
                             </div>
                             <div className="col-4">
-                                <button className="btn btn-dark btn-sm w-100 h-100" onClick={loadImage}>Get Image</button>
+                                <button className="btn btn-dark btn-sm w-100 h-100" onClick={getImage}>Get Images</button>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="col-lg-6 m-auto">
-                        <img className='w-100' id="getImage" src={search} alt="" />
+                    <div className="col-lg-6 m-auto text-center">
+                        <img id='searchSymbol' className='w-100 rounded' src={search} alt="" />
                     </div>
                 </div>
 
-                <div className="row">
-                    <div className="col-lg-6 m-auto">
-                        <button className="btn btn-secondary w-100 my-1" id="copyImgAdd" onClick={copyImageAddress}>Copy Image Address</button>
-                    </div>
-
+                <div className="row bg-secondary">
+                    {imageArray.map((element) => {
+                        return <div className="col-md-4" key={element.urls.raw}>
+                            <ImageItem rawUrl={element.urls.raw} />
+                        </div>
+                    })}
                 </div>
 
             </div>
---
+++ src/components/ImageItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+function ImageItem(props) {
+
+    function copyImageAddress() {
+        navigator.clipboard.writeText(props.rawUrl);
+    }
+
+    return (
+        <>
+            <div className="container bg-secondary bg-opacity-25 m-2 p-2 rounded border border-dark">
+                <div className="row">
+                    <div className="col">
+                        <img src={props.rawUrl} className='w-100' />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-6 m-auto">
+                        <button className="btn btn-dark w-100 my-1" id="copyImgAdd" onClick={copyImageAddress}>Copy Image Address</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ImageItem
